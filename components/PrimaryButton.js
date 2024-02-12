3a3d5ee21f5b1d0fae55a4d367d16d026269f31d
bernard rayoso <rayosobernard@gmail.com>
2024-02-12 21:26:31 +0800

    style Primary components, used Pressable on button, add styles for outer and inner container

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,12 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
 
-export default function PrimaryButton({children}) {
+export default function PrimaryButton({ children }) {
+  const pressHandler = () => {
+    console.log("press");
+  };
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#6400233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
