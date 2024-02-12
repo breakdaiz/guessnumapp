492d5c5a7cfca3f7be5ce1799a3564e709ab8456
bernard rayoso <rayosobernard@gmail.com>
2024-02-12 20:14:27 +0800

    Add button on StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,13 +4,20 @@ import PrimaryButton from '../components/PrimaryButton'
 
 export default function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput/>
       <PrimaryButton> Reset</PrimaryButton>
       <PrimaryButton> Confirm</PrimaryButton>
-      {/* add buttons */}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        marginTop: 100,
+        padding: 16,
+    }
+
+})
