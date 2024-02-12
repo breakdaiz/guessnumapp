07c40aaf5c1a05e5c3656d623010b0a205972de4
bernard rayoso <rayosobernard@gmail.com>
2024-02-12 20:47:08 +0800

    Style and add validation textInput component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,31 +1,45 @@
-import { StyleSheet, View, TextInput, Button } from 'react-native'
-import React from 'react'
-import PrimaryButton from '../components/PrimaryButton'
+import { StyleSheet, View, TextInput, Button } from "react-native";
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        keyboardType='number-pad'
+        maxLength={2}
+        style={styles.numberInput}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton> Reset</PrimaryButton>
       <PrimaryButton> Confirm</PrimaryButton>
     </View>
-  )
+  );
 }
 
-
 const styles = StyleSheet.create({
-    inputContainer: {
-        marginHorizontal: 24,
-        borderRadius: 8,
-        marginTop: 100,
-        padding: 16,
-        backgroundColor: '#72063c',
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.25
-
-    }
-
-})
+  inputContainer: {
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    color: "#ddb52f",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
