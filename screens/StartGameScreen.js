eb7fa44e131b1fe7612d959340b2455758e89eaa
bernard rayoso <rayosobernard@gmail.com>
2024-02-12 20:15:09 +0800

    create a style for primary button

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from '../components/PrimaryButton'
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput/>
+      <TextInput />
       <PrimaryButton> Reset</PrimaryButton>
       <PrimaryButton> Confirm</PrimaryButton>
     </View>
@@ -15,9 +15,17 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flex: 1,
+        marginHorizontal: 24,
+        borderRadius: 8,
         marginTop: 100,
         padding: 16,
+        backgroundColor: '#72063c',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+
     }
 
 })
