4f6cfda33e49870a488e13bfbfb4868e50a66e26
bernard rayoso <rayosobernard@gmail.com>
2024-02-16 18:25:15 +0800

    Create Title Component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Title from "../components/Title";
 
-export default function GameScreen() {
+export default function GameScreen({ title }) {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's guess </Text>
+      <Title> Opponent's guess</Title>
       <View>
         <Text>Higher or lower!</Text>
       </View>
@@ -15,6 +16,15 @@ export default function GameScreen() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    padding: 12,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#ddb52f",
     padding: 12,
   },
 });
